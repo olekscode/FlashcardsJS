b8c95ca4e5c5d351438ea8c7d0413e3761a6d309
olekscode <olk.zaytsev@gmail.com>
2020-04-10 23:18:05 +0200

    Updates

---
+++ precache-manifest.f14c267c08285043c78db4106bebc47c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9195e31b28ae79ffb644d14ce9a4691f",
+    "url": "/flashcards.js/index.html"
+  },
+  {
+    "revision": "5e9fbd86b67b023acd63",
+    "url": "/flashcards.js/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9e111be860d1edd384e4",
+    "url": "/flashcards.js/static/js/2.e573ae1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flashcards.js/static/js/2.e573ae1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9fbd86b67b023acd63",
+    "url": "/flashcards.js/static/js/main.4a63ab28.chunk.js"
+  },
+  {
+    "revision": "f4dadb10db2c09c22feb",
+    "url": "/flashcards.js/static/js/runtime-main.37143606.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flashcards.js/static/media/logo.5d5d9eef.svg"
+  }
+]);
